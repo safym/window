39d3e2a06d2d220ede9f53dd52672b5a4ad009f7
safym <osafym@gmail.com>
2022-05-11 23:54:52 +0300

    делаю окно воспроизведения аудио

--- scripts/click.js
+++ scripts/click.js
@@ -1,28 +1,31 @@
-import { addDrag } from './drag.js';
+import { addDrag } from "./drag.js";
 
-var $window = document.getElementById('window');
+var $window = document.getElementById("window");
 
 // THIS BULLSHIT IS BROKEN IN CHROME 
 // HOW?????
-document.addEventListener('click', function (e) {
+document.addEventListener("click", function (e) {
     console.log("addEventListener");
 
     // console.log(e.target);
+
+    // закрытие окон и открытие главного окна
     var parent = getParentWindow(e.target);
 
-    if (hasClass(e.target, 'openbutton')) {
+    if (hasClass(e.target, "openbutton")) {
         openWindow(e.target, parent);
     }
 
-    else if (hasClass(e.target, 'closebutton')) {
+    else if (hasClass(e.target, "closebutton")) {
             closeWindow(parent);
     }
 
-    var menuentry = checkParent(e.target, 'file');
-    var filename = getFileName(menuentry);
-    console.log(filename);
+    // создание окон файлов
+    var menuentry = checkParent(e.target, "file");
 
     if (menuentry) {
+        var filename = getFileName(menuentry);
+
         if (menuentry.id == "image") {
             console.log(filename);
             createWindowImage(menuentry);
@@ -34,13 +37,16 @@ document.addEventListener('click', function (e) {
             
         }
         if (menuentry.id == "music") {
-            
+            createWindowMusic(menuentry);
         }
         if (menuentry.id == "folder") {
             
         }
         
     }
+
+    // действия окна файла музыки
+    
 }, false);
 
 
@@ -73,7 +79,7 @@ function getFileName(elem) {
 
 function getParentWindow(elem) {
     var parent = elem.parentNode
-    while (parent.className != 'window') {
+    while (parent.className != "window") {
         parent = parent.parentNode;
 
         if (parent == null) {
@@ -114,98 +120,181 @@ function closeWindow(parentWindow){
 
     if (parentWindow == $window) {
          // создаем кнопку, которая открывает окно
-        var $openbutton = document.createElement('img')
+        var $openbutton = document.createElement("img")
         $openbutton.className = "openbutton";
         $openbutton.id = "openBtn" + parentWindow.id.toString();
         $openbutton.src = "res/icons/system/more.svg";
 
 
-        var $top = document.getElementById('top');
+        var $top = document.getElementById("top");
         $top.prepend($openbutton);
     }
 }
 
 function createWindowImage(elemFile) {
 
-    var $winfile = document.createElement('div');
+    var $winfile = document.createElement("div");
     $winfile.className = "window";
     $winfile.id = getIdToOpen(elemFile); 
 
     var randomtop = getRndInteger(150, 400);
     var randomleft = getRndInteger(1000, 1400);
     
-    $winfile.style.display = 'flex';
-    $winfile.style.flexDirection = 'column';
-    $winfile.style.width = '500px';
-    $winfile.style.height = '500px';
-    $winfile.style.position = 'absolute';
-    $winfile.style.top = randomtop.toString() + 'px';
-    $winfile.style.left = randomleft.toString() + 'px';
+    $winfile.style.display = "flex";
+    $winfile.style.flexDirection = "column";
+    $winfile.style.width = "500px";
+    $winfile.style.height = "500px";
+    $winfile.style.position = "absolute";
+    $winfile.style.top = randomtop.toString() + "px";
+    $winfile.style.left = randomleft.toString() + "px";
     $winfile.style.zIndex = 1000;
     
     $window.after($winfile);
 
-    var $headerfile = document.createElement('div');
+    var $headerfile = document.createElement("div");
     $headerfile.className = "header";
     
     $winfile.prepend($headerfile);
     addDrag($headerfile, getParentWindow($headerfile));
 
-    var $namefile = document.createElement('p');
+    var $namefile = document.createElement("p");
     $namefile.className = "namefile";
-    $namefile.textContent = elemFile.toString();
-    $namefile.style.width = '70%';
-    $namefile.style.display = 'flex';
-    $namefile.style.justifyContent = 'center';
+    $namefile.textContent = elemFile.id.toString();
+    $namefile.style.width = "90%";
+    $namefile.style.display = "flex";
+    $namefile.style.justifyContent = "center";
 
     $headerfile.prepend($namefile);
 
-    var $controlsfile = document.createElement('div');
+    var $controlsfile = document.createElement("div");
     $controlsfile.className = "controls";
 
     $headerfile.append($controlsfile);
 
-    var $closefile = document.createElement('div');
+    var $closefile = document.createElement("div");
     $closefile.className = "control";
 
     $controlsfile.append($closefile);
 
-    var $btnClosefile = document.createElement('img');
+    var $btnClosefile = document.createElement("img");
     $btnClosefile.className = "closebutton";
     $btnClosefile.src = "res/icons/system/close.svg";
 
     $closefile.append($btnClosefile);
 
-    // var $contentFile = document.createElement('div');
+    // var $contentFile = document.createElement("div");
     // $contentFile.className = "contentFile";
-    // $contentFile.display = 'flex';
+    // $contentFile.display = "flex";
 
-    // $contentFile.style.width = '100%';
-    // $contentFile.style.height = '100%';
-    // $contentFile.style.padding = '0';
+    // $contentFile.style.width = "100%";
+    // $contentFile.style.height = "100%";
+    // $contentFile.style.padding = "0";
 
     // $headerfile.after($contentFile);
 
-    var $contentFile = document.createElement('img');
+    var $contentFile = document.createElement("img");
     $contentFile.className = "contentFile";
-    $contentFile.display = 'flex';
+    $contentFile.id = "contentFileImage";
+    $contentFile.display = "flex";
 
-    $contentFile.style.width = '100%';
-    $contentFile.style.height = '100%';
-    $contentFile.style.padding = '0';
+    $contentFile.style.width = "100%";
+    $contentFile.style.height = "100%";
+    $contentFile.style.padding = "0";
     $contentFile.src = "res/content/image.jpg"
 
     $headerfile.after($contentFile);
 }
 
+function createWindowMusic(elemFile) {
+
+    // создание окна (основные параметры)
+    var $winfile = document.createElement("div");
+    $winfile.className = "window";
+    $winfile.id = getIdToOpen(elemFile); 
+
+    var randomtop = getRndInteger(150, 400);
+    var randomleft = getRndInteger(1000, 1400);
+    
+    $winfile.style.display = "flex";
+    $winfile.style.flexDirection = "column";
+    $winfile.style.width = "300px";
+    $winfile.style.height = "200px";
+    $winfile.style.position = "absolute";
+    $winfile.style.top = randomtop.toString() + "px";
+    $winfile.style.left = randomleft.toString() + "px";
+    $winfile.style.zIndex = 1000;
+    
+    $window.after($winfile);
+
+    //создание заголовка окна
+    var $headerfile = document.createElement("div");
+    $headerfile.className = "header";
+    
+    $winfile.prepend($headerfile);
+    addDrag($headerfile, getParentWindow($headerfile));
+
+    var $namefile = document.createElement("p");
+    $namefile.className = "namefile";
+    $namefile.textContent = elemFile.id.toString();
+    $namefile.style.width = "90%";
+    $namefile.style.display = "flex";
+    $namefile.style.justifyContent = "center";
+
+    $headerfile.prepend($namefile);
+
+    // создание кнопок окна
+    var $controlsfile = document.createElement("div");
+    $controlsfile.className = "controls";
+    
+    $headerfile.append($controlsfile);
+
+    var $closefile = document.createElement("div");
+    $closefile.className = "control";
+
+    $controlsfile.append($closefile);
+
+    var $btnClosefile = document.createElement("img");
+    $btnClosefile.className = "closebutton";
+    $btnClosefile.src = "res/icons/system/close.svg";
+
+    $closefile.append($btnClosefile);
+
+    // создание элемента аудио
+    var $contentFile = new Audio("res/content/ambhouse.mp3");
+    $contentFile.className = "contentFile";
+    $contentFile.id = "contentFileMusic";
+    $contentFile.preload = "none"
+    $contentFile.display = "flex";
+    addMusicPlay($contentFile);
+
+    $contentFile.style.width = "100%";
+    $contentFile.style.height = "100%";
+    $contentFile.style.padding = "0";
+    // $contentFile.src = "res/content/ambhouse.mp3"
+
+    $headerfile.after($contentFile);
+
+    // ДЕЛАЮ кнопки воспроизведения аудио
+    // var $btnPlayMusic = document.createElement("div");
+    // $btnClosefile.className = "button";
+    // $btnClosefile.src = "res/icons/system/close.svg";
+
+    // $closefile.append($btnClosefile);
+    
+}
+
 function getIdToOpen(elemFile) {
-    return "Win" + elemFile.toString();
+    return "Win" + elemFile.id.toString();
 }
 
 function splitWinIdToOpen(elemFile) {
-    return elemFile.id.replace('Win', '');
+    return elemFile.id.replace("Win", "");
 }
 
 function splitBtnIdToOpen(elemFile) {
-    return elemFile.id.replace('openBtn', '');
+    return elemFile.id.replace("openBtn", "");
+}
+
+function addMusicPlay(music) {
+    music.play()
 }
