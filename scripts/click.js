e7f9603504390ba4afb7156d311cb1e00e9ed3de
safym <osafym@gmail.com>
2022-05-17 23:33:11 +0300

    немного причесала js

--- scripts/click.js
+++ scripts/click.js
@@ -2,96 +2,47 @@ import { addDrag } from "./drag.js";
 
 var $window = document.getElementById("window");
 
-// THIS BULLSHIT IS BROKEN IN CHROME 
-// HOW?????
+// обработка события нажания (не работает в хроме :( ...)
 document.addEventListener("click", function (e) {
 
     // закрытие окон и открытие главного окна
-    var parent = getParentWindow(e.target);
+    var $parent = getParentWindow(e.target);
 
     if (hasClass(e.target, "openbutton")) {
-        openWindow(e.target, parent);
+        openWindow(e.target, $parent);
     }
 
     else if (hasClass(e.target, "closebutton")) {
-            closeWindow(parent);
+            closeWindow($parent);
     }
 
     // создание окон файлов
-    var menuentry = checkParent(e.target, "file");
+    var $menuentry = checkParent(e.target, "file");
 
-    if (menuentry) {
-        // console.log(menuentry);
+    if ($menuentry) {
 
-        var filename = getFileName(menuentry);
-
-        // console.log(filename);
-        createWindowImage(menuentry);
+        createWindowImage($menuentry);
 
     }
 
     
 }, false);
 
-
-function hasClass(elem, className) {
-    return elem.classList.contains(className);
-}
-
-function checkParent(elem, className) {
-    if (hasClass(elem,className)) {
-        return elem;
-    }
-
-    var parent = elem.parentNode
-    while (parent.className != className) {
-        parent = parent.parentNode;
-
-        if (parent == null) {
-            return false;
-        } 
-    }
-    return parent;
-}
-
-function getFileName(elem) {
-    var pChildren = elem.children[0].children[1].textContent;
-    console.log(pChildren)
-
-    return pChildren;
-
-
-}
-
-function getParentWindow(elem) {
-    var parent = elem.parentNode
-    while (parent.className != "window") {
-        parent = parent.parentNode;
-
-        if (parent == null) {
-            return false;
-        } 
-    }
-    return parent;
-}
-
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-}
-
+//открытие основного окна проводника //
 function openWindow(elem, parentWindow) {
 
-    var idToOpen = splitBtnIdToOpen(elem);
+    var $idToOpen = splitBtnIdToOpen(elem);
 
-    var openId = document.getElementById(idToOpen);
+    var $openId = document.getElementById($idToOpen);
         
-    openId.style.display = "grid";
+    $openId.style.display = "grid";
 
     // скрывает кнопку
-    var openButton = document.getElementById(elem.id);
-    openButton.style.display = "none";
+    var $openButton = document.getElementById(elem.id);
+    $openButton.style.display = "none";
 }
 
+// закрытие основного окна проводника//
 function closeWindow(parentWindow){
 
     // закрываем окно
@@ -110,26 +61,33 @@ function closeWindow(parentWindow){
     }
 }
 
-function createWindowImage(elemFile) {
+// создание новых окон //
 
+function createWindowImage(elemFile) {
+    
+    var $fileName = getFileName(elemFile)
+
+    //создание основы нового окна
     var $winfile = document.createElement("div");
+
     $winfile.className = "window";
     $winfile.id = getIdToOpen(elemFile); 
 
-    var randomtop = getRndInteger(150, 400);
-    var randomleft = getRndInteger(1000, 1400);
+    var $randomtop = getRndInteger(150, 400);
+    var $randomleftLeft = getRndInteger(1000, 1400);
     
     $winfile.style.display = "flex";
     $winfile.style.flexDirection = "column";
     $winfile.style.width = "500px";
     $winfile.style.height = "500px";
     $winfile.style.position = "absolute";
-    $winfile.style.top = randomtop.toString() + "px";
-    $winfile.style.left = randomleft.toString() + "px";
+    $winfile.style.top = $randomtop.toString() + "px";
+    $winfile.style.left = $randomleft.toString() + "px";
     $winfile.style.zIndex = 1000;
     
     $window.after($winfile);
 
+    //создание шапки нового окна
     var $headerfile = document.createElement("div");
     $headerfile.className = "header";
     
@@ -138,7 +96,7 @@ function createWindowImage(elemFile) {
 
     var $namefile = document.createElement("p");
     $namefile.className = "namefile";
-    $namefile.textContent = elemFile.id.toString();
+    $namefile.textContent = $fileName;
     $namefile.style.width = "90%";
     $namefile.style.display = "flex";
     $namefile.style.justifyContent = "center";
@@ -161,16 +119,7 @@ function createWindowImage(elemFile) {
 
     $closefile.append($btnClosefile);
 
-    // var $contentFile = document.createElement("div");
-    // $contentFile.className = "contentFile";
-    // $contentFile.display = "flex";
-
-    // $contentFile.style.width = "100%";
-    // $contentFile.style.height = "100%";
-    // $contentFile.style.padding = "0";
-
-    // $headerfile.after($contentFile);
-
+    //создание контента в новом окне
     var $contentFile = document.createElement("img");
     $contentFile.className = "contentFile";
     $contentFile.id = "contentFileImage";
@@ -179,22 +128,62 @@ function createWindowImage(elemFile) {
     $contentFile.style.width = "100%";
     $contentFile.style.height = "100%";
     $contentFile.style.padding = "0";
+    $contentFile.style.borderBottomLeftRadius = "10px";
+    $contentFile.style.borderBottomRightRadius = "10px";
 
-    var fileName = getFileName(elemFile)
-
-    $contentFile.src = "res/content/" + fileName; 
+    $contentFile.src = "res/content/" + $fileName; 
 
     $headerfile.after($contentFile);
 }
+
+// вспомогательные функции //
 
 function getIdToOpen(elemFile) {
     return "Win" + elemFile.id.toString();
 }
 
-function splitWinIdToOpen(elemFile) {
-    return elemFile.id.replace("Win", "");
-}
-
 function splitBtnIdToOpen(elemFile) {
     return elemFile.id.replace("openBtn", "");
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function hasClass(elem, className) {
+    return elem.classList.contains(className);
+}
+
+function checkParent(elem, className) {
+    if (hasClass(elem,className)) {
+        return elem;
+    }
+
+    var $parent = elem.parentNode
+    while ($parent.className != className) {
+        $parent = $parent.parentNode;
+
+        if ($parent == null) {
+            return false;
+        } 
+    }
+    return $parent;
+}
+
+function getFileName(elem) {
+    var $Children = elem.children[0].children[1].textContent;
+
+    return $Children;
+}
+
+function getParentWindow(elem) {
+    var $parent = elem.parentNode
+    while ($parent.className != "window") {
+        $parent = $parent.parentNode;
+
+        if ($parent == null) {
+            return false;
+        } 
+    }
+    return $parent;
 }
